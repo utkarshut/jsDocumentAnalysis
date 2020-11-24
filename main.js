d132738040ffef10659ed47fa0bfb8c93229a129
utkarsh bhardwaj <visitutkarsh634@gmail.com>
2020-11-24 19:56:10 +0530

    main.js

---
+++ main.js
@@ -0,0 +1,30 @@
+// api url 
+const api_url = 
+	"https://norvig.com/big.txt "; 
+
+// Defining async function 
+async function getapi(url) { 
+	
+	// Storing response 
+	const response = await fetch(url); 
+	
+	// Storing data in form of JSON 
+	var data = await response.json(); 
+	console.log(data); 
+	if (response) { 
+		hideloader(); 
+	} 
+	show(data); 
+} 
+// Calling that async function 
+getapi(api_url); 
+
+// Function to hide the loader 
+function hideloader() { 
+	document.getElementById('loading').style.display = 'none'; 
+} 
+// Function to define innerHTML for HTML table 
+function show(data) {  
+	// Setting innerHTML as tab variable 
+	document.getElementById("employees").innerHTML = data; 
+} 
