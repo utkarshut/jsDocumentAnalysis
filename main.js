92e99ad61232568557dc7ed8608b81c0f144da45
utkarsh bhardwaj <visitutkarsh634@gmail.com>
2020-11-24 20:08:50 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+// Function to hide the loader 
+function hideloader() { 
+	document.getElementById('loading').style.display = 'none'; 
+} 
+// Function to define innerHTML for HTML table 
+function show(data) {  
+	// Setting innerHTML as tab variable 
+	document.getElementById("employees").innerHTML = data; 
+} 
 // api url 
 const api_url = 
 	"https://norvig.com/big.txt "; 
